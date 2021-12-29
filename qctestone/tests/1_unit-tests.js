98b6f1fffc39e4a9c9bdef55963deca1346527df
widyaageng <ageng.setyatutuko@gmail.com>
2021-12-29 20:47:16 +0800

    Add: more unit tests (16) by following user story

--- qctestone/tests/1_unit-tests.js
+++ qctestone/tests/1_unit-tests.js
@@ -6,47 +6,164 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function () {
-    suite('Non-json test', function () {
-        test('getNum checker', function () {
-            let result = convertHandler.getNum('5/6');
-            assert.approximately(result, 0.83, 0.01);
-            assert.equal(convertHandler.getNum('5/5/5'), 'invalid number');
-        });
+    test('getNum checker#1', function () {
+        let result = convertHandler.getNum('5');
+        assert.equal(result, 5);
+    });
 
-        test('getUnit checker', function () {
-            let result = convertHandler.getUnit('LbS');
-            assert.equal(result, 'lbs');
-            assert.equal(convertHandler.getUnit('yada'), 'invalid unit');
-        });
+    test('getNum checker#2', function () {
+        let result = convertHandler.getNum('3.4235');
+        assert.approximately(result, 3.4235, 0.01);
+    });
 
-        test('getReturnUnit checker', function () {
-            let result = convertHandler.getReturnUnit('mi');
-            assert.equal(result, 'km');
-            assert.equal(convertHandler.getReturnUnit('yada'), 'invalid unit');
-        });
+    test('getNum checker#3', function () {
+        let result = convertHandler.getNum('5/6');
+        assert.approximately(result, 0.83, 0.01);
+    });
 
-        test('spellOutUnit checker', function () {
-            const unitPairSet = {
-                'gal': 'gallons',
-                'L': 'litres',
-                'mi': 'miles',
-                'km': 'kilometers',
-                'lbs': 'pounds',
-                'kg': 'kilograms'
-            };
+    test('getNum checker#4', function () {
+        let result = convertHandler.getNum('5.345/6.52');
+        assert.approximately(result, 0.81978, 0.01);
+    });
 
-            for (const [key, val] of Object.entries(unitPairSet)) {
-                assert.equal(convertHandler.spellOutUnit(key), val);
-            };
-        });
+    test('getNum checker#5', function () {
+        assert.equal(convertHandler.getNum('5/5/5'), 'invalid number');
+    });
 
-        test('getString checker', function () {
-            let result = convertHandler.getString('50/5.7', 'kG', '', '');
-            assert.approximately(result.initNum, 50 / 5.7, 0.1);
-            assert.equal(result.initUnit, 'kg');
-            assert.approximately(result.returnNum, 19.2, 0.3);
-            assert.equal(result.returnUnit, 'lbs');
-            assert.equal(convertHandler.getString('5/5/5', 'yada'), 'invalid number and unit');
-        });
+    test('getNum checker#6', function () {
+        assert.equal(convertHandler.getNum(''), 1);
+    });
+
+    test('getUnit checker#1', function () {
+        const validUnit = ['gal', 'L', 'mi', 'km', 'lbs', 'kg', 'invalid unit'];
+        const testUnit = ['GAL', 'l', 'mI', 'kM', 'lBs', 'kG', 'megacombocrap'];
+        for (const [index, element] of Object.entries(validUnit)) {
+            assert.equal(convertHandler.getUnit(testUnit[index]), element);
+        };
+    });
+
+    test('getUnit checker#2', function () {
+        assert.equal(convertHandler.getUnit('yada'), 'invalid unit');
+    });
+
+    test('getUnit checker#3', function () {
+        const testPairSet = {
+            'gal': 'L',
+            'L': 'gal',
+            'mi': 'km',
+            'km': 'mi',
+            'lbs': 'kg',
+            'kg': 'lbs',
+            'megacombocrap': 'invalid unit'
+        };
+
+        for (const [key, value] of Object.entries(testPairSet)) {
+            assert.equal(convertHandler.getReturnUnit(key), value);
+        };
+    });
+
+    test('getReturnUnit spellOut checker#1', function () {
+        const testPairSet = {
+            'gal': 'gallons',
+            'L': 'litres',
+            'mi': 'miles',
+            'km': 'kilometers',
+            'lbs': 'pounds',
+            'kg': 'kilograms',
+            'megacombocrap': undefined
+        };
+
+        for (const [key, value] of Object.entries(testPairSet)) {
+            assert.equal(convertHandler.spellOutUnit(key), value);
+        };
+    });
+
+    test('convert checker#1 gallons', function () {
+        const tempObject = {
+            initNum: 1,
+            initUnit: 'gal',
+            result: 3.78541,
+            resultUnit: 'L'
+        };
+        
+        let result = convertHandler.convert(tempObject.initNum, tempObject.initUnit);
+        assert.equal(result[tempObject.initUnit], tempObject.initNum);
+        assert.approximately(result[tempObject.resultUnit], tempObject.result, 0.01);
+    });
+    
+    test('convert checker#2 litres', function () {
+        const tempObject = {
+            initNum: 1,
+            initUnit: 'L',
+            result: 0.26417,
+            resultUnit: 'gal'
+        };
+        
+        let result = convertHandler.convert(tempObject.initNum, tempObject.initUnit);
+        assert.equal(result[tempObject.initUnit], tempObject.initNum);
+        assert.approximately(result[tempObject.resultUnit], tempObject.result, 0.01);
+    });
+
+    test('convert checker#3 miles', function () {
+        const tempObject = {
+            initNum: 1,
+            initUnit: 'mi',
+            result: 1.60934,
+            resultUnit: 'km'
+        };
+        
+        let result = convertHandler.convert(tempObject.initNum, tempObject.initUnit);
+        assert.equal(result[tempObject.initUnit], tempObject.initNum);
+        assert.approximately(result[tempObject.resultUnit], tempObject.result, 0.01);
+    });
+
+    test('convert checker#4 kilometres', function () {
+        const tempObject = {
+            initNum: 1,
+            initUnit: 'km',
+            result: 0.62137,
+            resultUnit: 'mi'
+        };
+        
+        let result = convertHandler.convert(tempObject.initNum, tempObject.initUnit);
+        assert.equal(result[tempObject.initUnit], tempObject.initNum);
+        assert.approximately(result[tempObject.resultUnit], tempObject.result, 0.01);
+    });
+
+    test('convert checker#5 kg', function () {
+        const tempObject = {
+            initNum: 1,
+            initUnit: 'kg',
+            result: 2.20462,
+            resultUnit: 'lbs'
+        };
+        
+        let result = convertHandler.convert(tempObject.initNum, tempObject.initUnit);
+        assert.equal(result[tempObject.initUnit], tempObject.initNum);
+        assert.approximately(result[tempObject.resultUnit], tempObject.result, 0.01);
+    });
+
+    test('convert checker#6 lbs', function () {
+        const tempObject = {
+            initNum: 1,
+            initUnit: 'lbs',
+            result: 0.45359,
+            resultUnit: 'kg'
+        };
+        
+        let result = convertHandler.convert(tempObject.initNum, tempObject.initUnit);
+        assert.equal(result[tempObject.initUnit], tempObject.initNum);
+        assert.approximately(result[tempObject.resultUnit], tempObject.result, 0.01);
+    });
+
+
+
+    test('getString checker', function () {
+        let result = convertHandler.getString('50/5.7', 'kG', '', '');
+        assert.approximately(result.initNum, 50 / 5.7, 0.1);
+        assert.equal(result.initUnit, 'kg');
+        assert.approximately(result.returnNum, 19.2, 0.3);
+        assert.equal(result.returnUnit, 'lbs');
+        assert.equal(convertHandler.getString('5/5/5', 'yada'), 'invalid number and unit');
     });
 });
--- qctestone/tests/2_functional-tests.js
+++ qctestone/tests/2_functional-tests.js
@@ -6,94 +6,92 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function () {
-    suite('JSON tests', function () {
-        test('10L good input convert', function () {
-            const tempDict = {
-                "reqParam": '?input=10L',
-                "initNum": 10,
-                "initUnit": "L",
-                "returnNum": 2.64172,
-                "returnUnit": "gal",
-                "string": "10 litres converts to 2.64172 gallons"
-            };
+    test('10L good input convert', function () {
+        const tempDict = {
+            "reqParam": '?input=10L',
+            "initNum": 10,
+            "initUnit": "L",
+            "returnNum": 2.64172,
+            "returnUnit": "gal",
+            "string": "10 litres converts to 2.64172 gallons"
+        };
 
-            chai
-                .request(server)
-                .get(`/api/convert${tempDict.reqParam}`)
-                .end(function (err, res) {
-                    assert.equal(res.body.initNum, tempDict.initNum);
-                    assert.equal(res.body.initUnit, tempDict.initUnit);
-                    assert.equal(res.body.returnNum, tempDict.returnNum);
-                    assert.equal(res.body.returnUnit, tempDict.returnUnit);
-                    assert.equal(res.body.string, tempDict.string);
-                });
-        });
+        chai
+            .request(server)
+            .get(`/api/convert${tempDict.reqParam}`)
+            .end(function (err, res) {
+                assert.equal(res.body.initNum, tempDict.initNum);
+                assert.equal(res.body.initUnit, tempDict.initUnit);
+                assert.equal(res.body.returnNum, tempDict.returnNum);
+                assert.equal(res.body.returnUnit, tempDict.returnUnit);
+                assert.equal(res.body.string, tempDict.string);
+            });
+    });
 
-        test('32g bad unit convert', function () {
-            const tempDict = {
-                "reqParam": '?input=32g',
-                "message": 'invalid unit'
-            };
+    test('32g bad unit convert', function () {
+        const tempDict = {
+            "reqParam": '?input=32g',
+            "message": 'invalid unit'
+        };
 
-            chai
-                .request(server)
-                .get(`/api/convert${tempDict.reqParam}`)
-                .end(function (err, res) {
-                    assert.equal(res.status, 200);
-                    assert.equal(res.body, tempDict.message);
-                });
-        });
+        chai
+            .request(server)
+            .get(`/api/convert${tempDict.reqParam}`)
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body, tempDict.message);
+            });
+    });
 
-        test('3/7.2/4kg bad number convert', function () {
-            const tempDict = {
-                "reqParam": '?input=3/7.2/4kg ',
-                "message": 'invalid number'
-            };
+    test('3/7.2/4kg bad number convert', function () {
+        const tempDict = {
+            "reqParam": '?input=3/7.2/4kg ',
+            "message": 'invalid number'
+        };
 
-            chai
-                .request(server)
-                .get(`/api/convert${tempDict.reqParam}`)
-                .end(function (err, res) {
-                    assert.equal(res.status, 200);
-                    assert.equal(res.body, tempDict.message);
-                });
-        });
+        chai
+            .request(server)
+            .get(`/api/convert${tempDict.reqParam}`)
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body, tempDict.message);
+            });
+    });
 
-        test('3/7.2/4kilomegagram bad number & input convert', function () {
-            const tempDict = {
-                "reqParam": '?input=3/7.2/4kilomegagram',
-                "message": 'invalid number and unit'
-            };
+    test('3/7.2/4kilomegagram bad number & input convert', function () {
+        const tempDict = {
+            "reqParam": '?input=3/7.2/4kilomegagram',
+            "message": 'invalid number and unit'
+        };
 
-            chai
-                .request(server)
-                .get(`/api/convert${tempDict.reqParam}`)
-                .end(function (err, res) {
-                    assert.equal(res.status, 200);
-                    assert.equal(res.body, tempDict.message);
-                });
-        });
+        chai
+            .request(server)
+            .get(`/api/convert${tempDict.reqParam}`)
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body, tempDict.message);
+            });
+    });
 
-        test('kg no number conver', function () {
-            const tempDict = {
-                "reqParam": '?input=kg',
-                "initNum": 1,
-                "initUnit": "kg",
-                "returnNum": 2.20462,
-                "returnUnit": "lbs",
-                "string": "1 kilograms converts to 2.20462 pounds"
-            };
+    test('kg no number conver', function () {
+        const tempDict = {
+            "reqParam": '?input=kg',
+            "initNum": 1,
+            "initUnit": "kg",
+            "returnNum": 2.20462,
+            "returnUnit": "lbs",
+            "string": "1 kilograms converts to 2.20462 pounds"
+        };
 
-            chai
-                .request(server)
-                .get(`/api/convert${tempDict.reqParam}`)
-                .end(function (err, res) {
-                    assert.equal(res.body.initNum, tempDict.initNum);
-                    assert.equal(res.body.initUnit, tempDict.initUnit);
-                    assert.equal(res.body.returnNum, tempDict.returnNum);
-                    assert.equal(res.body.returnUnit, tempDict.returnUnit);
-                    assert.equal(res.body.string, tempDict.string);
-                });
-        });
+        chai
+            .request(server)
+            .get(`/api/convert${tempDict.reqParam}`)
+            .end(function (err, res) {
+                assert.equal(res.body.initNum, tempDict.initNum);
+                assert.equal(res.body.initUnit, tempDict.initUnit);
+                assert.equal(res.body.returnNum, tempDict.returnNum);
+                assert.equal(res.body.returnUnit, tempDict.returnUnit);
+                assert.equal(res.body.string, tempDict.string);
+            });
     });
 });
