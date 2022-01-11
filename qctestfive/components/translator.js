a73fab11f673651759a384011db467a97dea9fe9
widyaageng <ageng.setyatutuko@gmail.com>
2022-01-11 23:31:08 +0800

    Add: translator.js functions to perform reverse lookup and translation direction

--- qctestfive/components/translator.js
+++ qctestfive/components/translator.js
@@ -3,8 +3,64 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
-class Translator {
+let britishToAmericanSpelling;
+let britishToAmericanTitles;
 
-}
+const BRITISHDIRECTION = 'american-to-british';
+const AMERICANDIRECTION = 'british-to-american';
+
+class Translator {
+    
+    constructor() {
+        this.britToAmericaSpelling = {};
+        this.britToAmericaTitles = {};
+    };
+
+    //has to be called for every Translator instance
+    initBritToAmericaSpelling() {
+        for (const [key, value] of Object.entries(americanToBritishSpelling)) {
+            this.britToAmericaSpelling[value] = key;
+        };
+    };
+
+    //has to be called for every Translator instance
+    initBritToAmericaTitles() {
+        for (const [key, value] of Object.entries(americanToBritishTitles)) {
+            this.britToAmericaTitles[value] = key;
+        };
+    };
+
+    //has to be called before translating
+    getDirection(payloadLocale) {
+        if (payloadLocale === BRITISHDIRECTION) {
+            return {
+                spelling: americanToBritishSpelling,
+                titles: americanToBritishTitles,
+                self: britishOnly
+            };
+        } else if (payloadLocale === AMERICANDIRECTION) {
+            return {
+                spelling: this.britToAmericaSpelling,
+                titles: this.britishToAmericanTitles,
+                self: americanOnly
+            };
+        };
+    };
+
+
+    toLowerCaseArray(text) {
+        let cleanedUpText = text.match(/^(.*)[!?.]+$/)[1];
+
+        return cleanedUpText.toLowerCase().split(/\s+/);
+    };
+
+
+    findPair(lowerCaseArray, direction) {
+        for (let word of lowerCaseArray) {
+
+        }
+    };
+
+};
 
 module.exports = Translator;
